004dc033f9b20795a20d0b34aa61068f76b9d53e
EC2 Default User <ec2-user@ip-172-31-5-36.ap-northeast-1.compute.internal>
2023-02-13 16:20:18 +0000

    [Add]トップページの更新

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,4 @@
 class HomesController < ApplicationController
-   require 'themoviedb-api'
-  Tmdb::Api.key("a882fb831bd72865c87e32d59c65bdda")
-  Tmdb::Api.language("ja")
-
   def top
   end
 
--- app/javascript/src/tmdb.js
+++ app/javascript/src/tmdb.js
@@ -1,33 +1,35 @@
+window.addEventListener('load', function(){
 // 定数
 // Discover
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 // Images
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 // Search
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 // 要素を取得
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
+// 要素を取得
 // 映画情報を取得
-getMovies(API_URL);
+getMovies(API_URL)
 
 // async/awaitで非同期処理
 async function getMovies(url) {
-// APIヘGETリクエスト
+  // APIヘGETリクエスト
 /*global fetch*/
-const res = await fetch(url);
-// 取得したデータをJSON形式で取得
-const data = await res.json();
-// Movieを表示
-  showMovies(data.results);
+  const res = await fetch(url)
+  // 取得したデータをJSON形式で取得
+  const data = await res.json()
+  /*global data*/
+  showMovies(data.results)
 }
 
 function showMovies(movies) {
   // 画面初期化
-  main.innerHTML = '';
+  main.innerHTML = ''
 
   movies.forEach((movie) => {
     // オブジェクトから各変数に格納
@@ -36,36 +38,31 @@ function showMovies(movies) {
       poster_path,
       vote_average,
       overview
-    } = movie;
+    } = movie
 
-    // Movieパネルを作成
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
+    // topページ用
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
     // MoveパネルをHTMLに埋め込む
-    movieEl.innerHTML =  `
-      <img src="${IMG_PATH + poster_path}" alt="${title}">
-      <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-      </div>
-      <div class="overview">
-        <h3>Overview</h3>
-        ${overview}
-      </div>
-    `;
-    main.appendChild(movieEl);
-  });
-
+    movieEl.innerHTML =
+    `
+     <div class="col-md-3 offset-md-1 d-inline-block text-truncate" style="max-width: 230px;">
+      <img src="${IMG_PATH + poster_path}" alt="${title}" width="${200}" margin="${10}">
+      <a href="/public/movies/:id">${title}</a>
+     </div>
+    `
+    main.appendChild(movieEl)
+  })
 }
 
-// 人気順にクラス分けをする
+  // 人気順にクラス分けをする
 function getClassByRate(vote) {
   if(vote >= 8) {
-    return 'green';
+    return 'green'
   } else if(vote >= 5) {
-    return 'orangge';
+    return 'orangge'
   } else {
-    return 'red';
+    return 'red'
   }
 }
 
@@ -73,21 +70,21 @@ function getClassByRate(vote) {
 form.addEventListener('submit', (e) => {
   // フォームのデフォルトの動きを禁止
   // ここではページのリダイレクトをキャンセルしている
-  e.preventDefault();
+  e.preventDefault()
 
   // 検索文字列取得
-  const searchTerm = search.value;
+  const searchTerm = search.value
 
   if(search && searchTerm !== '') {
     // 検索文字列を元に検索をする
-    getMovies(SEARCH_API + searchTerm);
+    getMovies(SEARCH_API + searchTerm)
     // 検索文字列を削除
-    search.value = '';
-  }
-  else {
+    search.value = ''
+  }else {
     // ページを再読み込み
     // 検索キーワードない状態で検索すると初期状態のデータを表示できる
     // Searchで検索した結果をクリアしたい時などに使える
-    window.location.reload();
+    window.location.reload()
   }
+})
 })
