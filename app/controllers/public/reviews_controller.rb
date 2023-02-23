685dd9c966766caedc5a025afb617d3e8bf59ac9
EC2 Default User <ec2-user@ip-172-31-5-36.ap-northeast-1.compute.internal>
2023-02-23 14:57:32 +0000

    [Add]レビュー新規作成画面の更新

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,7 +1,23 @@
 class Public::ReviewsController < ApplicationController
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to public_movie_path(@review.movie_id)
+    else
+      render top
+    end
   end
 
   def show
+  end
+
+  private
+
+  def review_params
+ params.require(:review).permit(:title, :review, :evaluation)
   end
 end
