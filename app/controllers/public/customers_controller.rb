cad64020300a2e1351e0d0b32af930ac5e7925b2
EC2 Default User <ec2-user@ip-172-31-5-36.ap-northeast-1.compute.internal>
2023-03-01 16:46:03 +0000

    [Add]ユーザー情報編集機能の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,23 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if Customer.update(customer_params)
+      redirect_to public_customer_path(current_customer.id)
+    else
+      render :root
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :email, :self_introduction)
   end
 end
